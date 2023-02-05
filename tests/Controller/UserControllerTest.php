4143feacb51decb1606df935e55c140e424e5da6
vcarine <you@example.com>
2023-02-05 17:33:54 +0100

    testListOfUsersForAdmin ok

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -16,44 +16,45 @@ class UserControllerTest extends WebTestCase
     /**
      * @throws \Exception
      */
-//    public function testListOfUsersForAdmin(): void
-//    {
-//        $client = static::createClient();
-//        $user = static::getContainer()->get(UserRepository::class)->findOneByUsername('admin');
-////        dd($user);
-//        $client->loginUser($user);
-//        $client->request('GET', '/users');
+    public function testListOfUsersForAdmin(): void
+    {
+        $client = static::createClient();
+        $user = static::getContainer()->get(UserRepository::class)->findOneByUsername('admin');
+        $client->loginUser($user);
+        $client->request('GET', '/users');
+        $this->assertResponseIsSuccessful();
+
 //        $this->assertResponseRedirects();
 //        $client->followRedirect();
 //        $this->assertRouteSame('user_list');
-//    }
+    }
 
     /**
      * @throws \Exception
      */
-//    public function testCreateUser(): void
-//    {
-//        $client = static::createClient();
-//        $user = static::getContainer()->get(UserRepository::class)->findOneByUsername('admin');
-////        dd($user);
-//        $client->loginUser($user);
-//        $crawler = $client->request(Request::METHOD_GET, '/users/create');
-////         dd($client->getResponse());
-////        echo $client->getResponse()->getContent();
-//
-//        $this->assertInstanceOf(Form::class,
-//            $crawler->selectButton('Ajouter')->form());
-//
-//        $client->submitForm('Ajouter', [
-//            'user[username]' => 'user2',
-//            'user[password][first]' => 'password',
-//            'user[password][second]' => 'password',
-//            'user[email]' => 'user2@gmail.com',
-//        ]);
-//        $this->assertResponseRedirects();
-//        $client->followRedirect();
-//        $this->assertRouteSame('user_list');
-//    }
+    public function testCreateUser(): void
+    {
+        $client = static::createClient();
+        $user = static::getContainer()->get(UserRepository::class)->findOneByUsername('admin');
+//        dd($user);
+        $client->loginUser($user);
+        $crawler = $client->request(Request::METHOD_GET, '/users/create');
+//         dd($client->getResponse());
+//        echo $client->getResponse()->getContent();
+
+        $this->assertInstanceOf(Form::class,
+            $crawler->selectButton('Ajouter')->form());
+
+        $client->submitForm('Ajouter', [
+            'user[username]' => 'user2',
+            'user[password][first]' => 'password',
+            'user[password][second]' => 'password',
+            'user[email]' => 'user2@gmail.com',
+        ]);
+        $this->assertResponseRedirects();
+        $client->followRedirect();
+        $this->assertRouteSame('user_list');
+    }
 
     /**
      * @throws \Exception
