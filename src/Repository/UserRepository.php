31fc59b4dbf6cd80c3c6e84c6d75ee4dc9392c51
vcarine <you@example.com>
2023-02-08 15:34:05 +0100

    update

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -5,8 +5,7 @@ namespace App\Repository;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+
 
 /**
  * @extends ServiceEntityRepository<User>
@@ -40,14 +39,5 @@ class UserRepository extends ServiceEntityRepository
             $this->getEntityManager()->flush();
         }
     }
-    public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
-    {
-        if (!$user instanceof User) {
-            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', \get_class($user)));
-        }
 
-        $user->setPassword($newHashedPassword);
-
-        $this->save($user, true);
-    }
 }
