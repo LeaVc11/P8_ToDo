f91134fc8eacaa11b1ddfc76b11892687b898563
vcarine <you@example.com>
2023-02-09 09:35:45 +0100

    update

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -44,7 +44,7 @@ class UserController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
             $this->addFlash('success', "L'utilisateur a bien été ajouté.");
-            return $this->redirectToRoute('user_list');
+            return $this->redirectToRoute('homepage');
         }
         return $this->render('user/create.html.twig', [
             'form' => $form->createView(),
