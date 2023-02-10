1e63944eea15e143e3647a447599fa18621cad24
vcarine <you@example.com>
2023-02-10 10:57:58 +0100

    create TestRepository

---
+++ tests/Repository/UserRepositoryTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Tests\Repository;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class UserRepositoryTest extends KernelTestCase
+{
+    public function testRemove(): void
+    {
+        $userRepository = new UserRepository(static::getContainer()->get(ManagerRegistry::class));
+
+        $user = $userRepository->findOneBy(['username' => 'user']);
+        $this->assertInstanceOf(User::class, $user);
+
+        $userRepository-> remove($user, true);
+
+        $this->assertNull($userRepository->findOneBy(['username' => 'user']));
+    }
+}
