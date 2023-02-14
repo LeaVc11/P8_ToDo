2e6632dda76fc23b9c7e4ac8eccb7181fb315c8b
vcarine <you@example.com>
2023-02-14 09:33:35 +0100

    update DateTime

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -10,13 +10,18 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: TaskRepository::class)]
 class Task
 {
+
+    public function __construct()
+    {
+        $this->createdAt = new DateTime();
+    }
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-//    #[ORM\Column(options: ['default' => 'now'])]
-//    private ?DateTime $createdAt;
+    #[ORM\Column(options: ['default' => 'now'])]
+    private ?DateTime $createdAt;
 
     #[ORM\Column(length: 255)]
     private ?string $title = null;
@@ -30,27 +35,28 @@ class Task
     #[ORM\ManyToOne(inversedBy: 'tasks')]
     private ?User $user = null;
 
-//    public function __construct()
-//    {
-//        $this->createdAt = new DateTime();
-//    }
+    /**
+     * @return DateTime|null
+     */
+    public function getCreatedAt(): ?DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param DateTime|null $createdAt
+     */
+    public function setCreatedAt(?DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-//    public function getCreatedAt(): ?\DateTime
-//    {
-//        return $this->createdAt;
-//    }
-//
-//    public function setCreatedAt(\DateTime $createdAt): self
-//    {
-//        $this->createdAt = $createdAt;
-//
-//        return $this;
-//    }
 
     public function getTitle(): ?string
     {
